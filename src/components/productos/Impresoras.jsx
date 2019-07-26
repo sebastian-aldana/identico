32ee2bdad9050b496e9ce59ba84e7346c968603b
sebastian aldana <sebastinald98@gmail.com>
2019-07-25 21:21:51 -0500

    agregando componente de impresoras

--- src/components/productos/Impresoras.jsx
+++ src/components/productos/Impresoras.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import logo_zebra from "../../img/logo-zebra.jpg";
+import logo_fargo from "../../img/logo-fargo.jpg";
+
+import AOS from "aos";
 
 const Impresoras = () => {
-  return <h1>Este es el componente de las impresoras</h1>;
+  const [active, setActive] = useState(1);
+  const data = require("../../json/productos.json");
+  var numero = 0;
+  AOS.init();
+  return (
+    <div
+      class="tab-pane fade show active p-3 p-md-5 text-justify"
+      id="lealtad"
+      role="tabpanel"
+      aria-labelledby="lealtad-tab"
+    >
+      <div class="row justify-content-center">
+        <div class="col-11 text-center text-md-left">
+          <span class="mx-2">
+            <img src={logo_zebra} class="img-fluid" />
+          </span>
+          <span class="mx-2">
+            <a
+              href="http://identicomexico.com/impresoras-fargo#impresoras-fargo"
+              class="btn btn-link"
+              onClick={e => {
+                e.preventDefault();
+                setActive(2);
+              }}
+            >
+              <img src={logo_fargo} class="img-fluid" />
+            </a>
+          </span>
+        </div>
+      </div>
+
+      <div class="row productos" id="show_filtro_marca_impresoras">
+        <div class="col-12">{`Productos`}</div>
+        {data.data.map(data => {
+          if (data.id_marca == active && data.id_tipo == 1) {
+            numero = data.length;
+            return (
+              <>
+                <div
+                  data-aos="fade-up"
+                  data-aos-duration="1000"
+                  class="col-md-6 col-lg-3 text-center p-3 mb-5 pb-5"
+                >
+                  <a href="ling">
+                    <img
+                      src={require("../../img/fotos_productos/" + data.img)}
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </a>
+                  <a href="link">
+                    <h6 class="bAculC">
+                      <span class="py-4 d-inline-block text-white">
+                        {data.nombre}
+                      </span>
+                    </h6>
+                  </a>
+                </div>
+              </>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Impresoras;
