81b1377131761c5120138cd289c54a54f9c280ae
sebastian aldana <sebastinald98@gmail.com>
2019-08-17 12:24:54 -0500

    modificando el servidor

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const express = require("express");
-const App = require("./identico/ssr/index");
-const ReactRouter = require("react-router");
+import express from "express";
+import React from "react";
+import App from "./identico/ssr/index";
+import { StaticRouter } from "react-router";
+import reactDOMServer from "react-dom/server";
 const app = express();
-
-<ReactRouter.StaticRouter>
-  <App />
-</ReactRouter.StaticRouter>;
-
 // app.use(express.static("identico"));
 
 app.get("*", (req, res) => {
   console.log(req.url);
+  const html = reactDOMServer.renderToString(
+    <StaticRouter location={req.url}>
+      <App />
+    </StaticRouter>
+  );
   res.write(`Hola mundo desde ${req.url}`);
   // res.sendFile(__dirname + "/identico/index.html");
   res.end();
 });
 
 app.listen(3000);
+console.log("estamos encendidos");
