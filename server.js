2487f750f8fee0f58b129b96afdbf3b09e3b16b8
sebastian aldana <sebastinald98@gmail.com>
2019-08-17 11:03:46 -0500

    instalando la dependencia de react-router para el server

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-var express = require("express");
-var app = express();
-app.use(express.static("identico"));
+const express = require("express");
+const App = require("./identico/ssr/index");
+const ReactRouter = require("react-router");
+const app = express();
+
+<ReactRouter.StaticRouter>
+  <App />
+</ReactRouter.StaticRouter>;
+
+// app.use(express.static("identico"));
 
 app.get("*", (req, res) => {
   console.log(req.url);
-  // res.write(`Hola mundo desde ${req.url}`);
-  res.sendFile(__dirname + "/identico/index.html");
+  res.write(`Hola mundo desde ${req.url}`);
+  // res.sendFile(__dirname + "/identico/index.html");
   res.end();
 });
 
