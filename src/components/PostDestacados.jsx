64b68c3de4e3b5b2a91180c46aa9dc156a339bc9
sebastian aldana <sebastinald98@gmail.com>
2019-08-18 11:50:47 -0500

    maquetando componente de PostDestacados

--- src/components/PostDestacados.jsx
+++ src/components/PostDestacados.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import "../css/postdestacados.css";
+import uno from "../post/uno.jpg";
+import { Link } from "react-router-dom";
 
 const PostDestacados = () => {
   return (
     <div className="destacados">
-      <h1>somo los post destacados</h1>
+      <div className="destacados-container">
+        <h6 className="destacados-title">Ultimos Post</h6>
+        <div className="imagen" />
+        <p className="texto centro">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          laboriosam cupiditate atque quas veniam dolor temporibus quidem
+          debitis doloremque itaque? Vero, perferendis. Odio pariatur nihil
+          veritatis corrupti aspernatur dignissimos doloremque.
+        </p>
+        <div className="boton">
+          <Link
+            className="btn btn-primary boton-position "
+            to="/"
+            // onClick={props.state(props.dato.id)}
+            role="button"
+          >
+            <p>Más información</p>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
