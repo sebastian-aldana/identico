7fa5530347dc18927499350e49a6e94703426d4b
sebastian aldana <sebastinald98@gmail.com>
2019-08-18 21:21:27 -0500

    agregando documentos y json ala pagina

--- src/components/GridPost.jsx
+++ src/components/GridPost.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 const GridPost = () => {
+  const post = require("../json/posts.json");
   return (
     <>
-      <div className="item">
+      {post.data.map(dato => {
+        return (
+          <div className="item">
+            <div className="titulos" key={dato.id}>
+              <h6>{dato.categoria}</h6>
+              <p>{dato.title}</p>
+            </div>
+          </div>
+        );
+      })}
+      {/* <div className="item">
         <div className="titulos">
           <h6>Categoria</h6>
           <p>Titulo</p>
@@ -26,7 +37,7 @@ const GridPost = () => {
           <h6>Categoria</h6>
           <p>Tiyulo</p>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
