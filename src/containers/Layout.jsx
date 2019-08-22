02861de186acd0b5c9d431930557130b42c2745d
sebastian aldana <sebastinald98@gmail.com>
2019-08-22 17:08:30 -0500

    colocnado paga con tu tarjeta de credito en el componente footer

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
 import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import Footer from "../components/Footer/Footer";
 
 const Layout = props => {
   return (
@@ -15,6 +16,7 @@ const Layout = props => {
       <Carrousel />
       {props.children}
       <Head />
+      <Footer />
     </>
   );
 };
