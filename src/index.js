6d533606c538504ed743ed9eac1461c4d82a9ae6
sebastian aldana <sebastinald98@gmail.com>
2019-09-05 14:54:51 -0500

    creando rutas para el servidor y agregando la historia

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
+
 import App from "./app";
 const app = document.getElementById("app");
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Router history={history}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Router>,
   app
 );
